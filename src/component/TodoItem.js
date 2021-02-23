7cd2d07dce7680c73c961de7ba804f251887c1d7
sabina <sabieena>
2021-02-23 15:28:52 +0545

    created react project todolist

---
+++ src/component/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TodoItem() {
+    return (
+        <div className="todo-item">
+            <input type="checkbox" />
+            <p>List Items</p>
+        </div>
+    )
+}
+
+export default TodoItem
