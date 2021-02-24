f24001456b69579f1781166ae6f63ff0157aba25
Sabieena <sabina.bhattrai2015@gmail.com>
2021-02-24 14:24:41 +0545

    mapping components

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import React, { Component } from 'react'
 import TodoItem from './component/TodoItem'
 import Practice from './component/Practice'
 import Test from './component/Test'
+import products from './component/ProductData'
+import Product from './component/Product'
 
-class App extends Component {
-  render() {
+
+function App() {
+  const productComponents = products.map(items => <Product key={items.id} product={items} />)
+  
     return (
       <div className="todo-list">
+        {productComponents}
         {/* <Practice />
         <TodoItem />
         <TodoItem />
         <TodoItem />
         <TodoItem /> */}
-        <Test
+        {/* <Test
           question="Why does Waldo wear stripes?" punchline="Because he doesn’t want to be spotted.." 
         />
         <Test
            punchline="Hello I am blah blah.." 
-        />
+        /> */}
+
+        
 
       </div>
     )
-  }
+  
 }
 
 export default App
