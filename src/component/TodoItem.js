2ceea9e1d4a2e3c863c7345b57bca3b4672ef8ca
sabina <sabieena>
2021-02-24 09:51:11 +0545

    use of state

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -3,6 +3,7 @@ import React from 'react'
 function TodoItem() {
     return (
         <div className="todo-item">
+            
             <input type="checkbox" />
             <p>List Items</p>
         </div>
